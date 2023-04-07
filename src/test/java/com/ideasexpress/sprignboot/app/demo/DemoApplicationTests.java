e4faa8ea31c1a531c6b7c6f73db69453ec03d307
Goku3 <eltiobryanz@gmail.com>
2023-04-07 16:59:44 -0500

    sin advertencias

--- src/test/java/com/ideasexpress/sprignboot/app/demo/DemoApplicationTests.java
+++ src/test/java/com/ideasexpress/sprignboot/app/demo/DemoApplicationTests.java
@@ -1,27 +1,11 @@
 package com.ideasexpress.sprignboot.app.demo;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.ideasexpress.sprignboot.app.demo.Models.Entity.Usuario;
-import com.ideasexpress.sprignboot.app.demo.Models.Repository.IUsuarioRepo;
+
 
 @SpringBootTest
 class DemoApplicationTests {
-	@Autowired
-	private IUsuarioRepo repo;
-	@Test
-	void crearUsuarioTest() {
-		Usuario usuario = new Usuario();
-		usuario.setId(1);
-		usuario.setNombre("Brian");
-		usuario.setClave("123");
-		Usuario retorno =repo.save(usuario);
-
-		assertTrue(retorno.getClave().equalsIgnoreCase(usuario.getClave()));
-	}
 
 }
